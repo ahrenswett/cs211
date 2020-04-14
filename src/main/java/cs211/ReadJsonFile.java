e5a4a61daa6473d42a889108d1d775ed9d18118c
ahren <ahrenswett@gmail.com>
2020-04-13 22:42:21 -0700

    added onto an assignment Added an arraylist populated by a Txt file added the ability for user input

--- src/main/java/cs211/ReadJsonFile.java
+++ src/main/java/cs211/ReadJsonFile.java
@@ -3,6 +3,7 @@ package ReadJsonFile;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,41 +11,41 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 class ReadJsonFile {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         //JSON parser object to parse read file
         JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader("student.json")){
-            //Read JSON file
-            Object obj = jsonParser.parse(reader);
-            JSONArray studentList = (JSONArray) obj;
-            System.out.println(studentList);
-            //Iterate over student array
-            studentList.forEach( std -> parseStudentObject( (JSONObject) std ));
+
+        String country="Hungary";
+
+        try {
+            Object obj = jsonParser.parse(new FileReader("covid19.json"));
+            JSONObject jsonObject = (JSONObject)obj;
+            JSONArray virus = (JSONArray)jsonObject.get(country);
+            System.out.println(country);
+
+            if (virus!=null) {
+                Iterator it = virus.iterator();
+                while (it.hasNext()) {
+                    JSONObject daily = (JSONObject)it.next();
+                    System.out.println("date: "+daily.get("date"));
+                    System.out.println("confirmed: "+daily.get("confirmed"));
+                    System.out.println("deaths: "+daily.get("deaths"));
+                    System.out.println("recovered: "+daily.get("recovered"));
+                }
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (ParseException e) {
+            e.printStackTrace();
+        } catch(NullPointerException e) {
             e.printStackTrace();
         }
     }
 
-    private static void parseStudentObject(JSONObject student){
-        //Get student object within list
-        JSONObject studentObject = (JSONObject)student.get("student");
-        //Get student first name
-        String firstName = (String) studentObject.get("firstName");
-        System.out.println(firstName);
-        //Get student last name
-        String lastName = (String) studentObject.get("lastName");
-        System.out.println(lastName);
-        //Get student high school
-        String highschool = (String) studentObject.get("highschool");
-        System.out.println(highschool);
-        // Get student home town
-        String hometown = (String) studentObject.get("hometown");
-        System.out.println(hometown);
-    }
+
 
 
 }
---
+++ src/main/java/cs211/ReadingURLJSON.java
@@ -0,0 +1,110 @@
+package cs211;
+/*
+Ahren Swett 4/13/2020
+thought the "(null != (str.readLine())" was a cool bit of code had to think about it for a minute
+figured I'd throw in some data normalization as well :)
+ */
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ReadingURLJSON {
+//    class var to decide if it should ask user for more input
+    private static Boolean askForMoreInput = true;
+    private static String country="";
+    private static ArrayList<String> countryList;
+
+
+    private static String prompt(BufferedReader reader, ArrayList<String> list) throws IOException {
+        System.out.println("Please enter a country whose Covid19 information you would like to view.");
+        String input = reader.readLine();
+        if(!list.contains(input)){
+           System.out.println("please check your spelling no country found");
+        }
+        return input;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        countryList = new ArrayList<>();
+        //              get user input
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            URL url = new URL("https://pomber.github.io/covid19/timeseries.json");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String str = "";
+            while (null != (str = br.readLine())) {
+                builder.append(str + "\n");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+//        Read each line of the country text and save it to the ArrayList
+        BufferedReader countryReader = new BufferedReader(new FileReader("country.txt"));
+//        make sure the next line is not null then add the country to the ArrayList
+        while (null != (country = countryReader.readLine())) {
+            countryList.add(country);
+        }
+
+
+//        figure out what country to query
+
+        while (askForMoreInput == true) {
+            country = prompt(userInput, countryList);
+            JSONParser jsonParser = new JSONParser();
+            Object obj = jsonParser.parse(builder.toString());
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray virus = (JSONArray) jsonObject.get(country);
+
+            System.out.println(country);
+
+            if (virus != null) {
+                Iterator it = virus.iterator();
+                int tot = 0;
+                while (it.hasNext()) {
+                    JSONObject daily = (JSONObject) it.next();
+                    System.out.println();
+                    System.out.println("date: " + daily.get("date"));
+                    System.out.println("confirmed: " + daily.get("confirmed"));
+                    System.out.println("deaths: " + daily.get("deaths"));
+                    System.out.println("recovered: " + daily.get("recovered"));
+                }
+            }
+//            askAgain();
+        }
+    }
+
+
+    private static void askAgain() throws IOException {
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Would you like information on another country? please enter Y or N");
+        String choice = userInput.readLine();
+
+//        TODO: Figure out why this is an endless loop
+        while(!choice.equalsIgnoreCase("Y") || !choice.equalsIgnoreCase("N")){
+            System.out.println("Would you like information on another country? please enter Y or N");
+            choice = userInput.readLine().toUpperCase();
+        }
+
+//        take care of the users choice
+        if (choice.toUpperCase() == "Y"){
+            prompt(userInput,countryList);
+        }else{
+            askForMoreInput = false;
+        }
+    }
+
+}
